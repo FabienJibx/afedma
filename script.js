30fc48f67f6a058d03ec7f43d28e0b8fcc9b33d5
FabienJibx <110095917+FabienJibx@users.noreply.github.com>
2023-04-07 10:54:42 +0200

    Static Website V1
    Raw version without components reuse

---
+++ script.js
@@ -0,0 +1,17 @@
+let img1=document.getElementsByClassName("bigger_image_background");
+console.log(img1);
+function openImg1(){
+    img1[0].style.display="flex";
+}
+function closeImg1(){
+    img1[0].style.display="none";
+}
+
+let img2=document.getElementsByClassName("bigger_image_background2");
+console.log(img2);
+function openImg2(){
+    img2[0].style.display="flex";
+}
+function closeImg2(){
+    img2[0].style.display="none";
+}
